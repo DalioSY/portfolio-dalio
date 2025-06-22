9a0d14e6d7189efeafa21183d389bc5cde9cb6be
DalioSY <dalioyamada@gmail.com>
2025-06-21 23:51:01 -0300

    img

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,11 +12,13 @@ type ProjectProps = {
 //https://api.github.com/repos/DalioSY/front-end-codeburger/languages
 
 export function Card({ item }: ProjectProps) {
+
+  const branch = 'master' || 'main'
   return (
     <div className='flex flex-col justify-between p-4 gap-3 rounded border bg-background'>
       <div className='w-full h-28  flex items-center justify-center'>
         <img
-          src={`https://raw.githubusercontent.com/DalioSY/${item.name}/refs/heads/master/img/git-1.png`}
+          src={`https://raw.githubusercontent.com/DalioSY/${item.name}/refs/heads/${branch}/img/git-1.png`}
           alt='foto projeto'
           className='h-full w-auto'
         />
@@ -54,9 +56,8 @@ export function Card({ item }: ProjectProps) {
           GitHub
         </a>
         <a
-          className={` flex border bg-background rounded-full py-1 px-3 hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:text-white active:scale-90 ${
-            item.homepage === null ? 'hidden' : 'item.homepage'
-          } ${item.homepage === '' ? 'hidden' : 'item.homepage'}`}
+          className={` flex border bg-background rounded-full py-1 px-3 hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:text-white active:scale-90 ${item.homepage === null ? 'hidden' : 'item.homepage'
+            } ${item.homepage === '' ? 'hidden' : 'item.homepage'}`}
           href={item.homepage}
           target='_blank'
         >
