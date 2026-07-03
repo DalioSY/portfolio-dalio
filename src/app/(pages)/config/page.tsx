6c90fb692062386dd548aa4816ecb9aa79f10b5a
DalioSY <dalioyamada@gmail.com>
2026-07-02 23:25:17 -0300

    button

--- src/app/(pages)/config/page.tsx
+++ src/app/(pages)/config/page.tsx
@@ -1,11 +1,11 @@
-import { ModelDark } from '@/components/Modeldark';
+import { Button } from '@/components/ui/button';
 
 export default function Config() {
   return (
     <div className=' m-20 p-5 flex flex-col items-center justify-center bg-background rounded-xl'>
       <div className='flex items-center gap-5'>
         <p>Tema: Claro/Escuro</p>
-        <ModelDark />
+        <Button themeDark />
       </div>
     </div>
   );
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,57 +1,39 @@
-import * as React from 'react';
-import { Slot } from '@radix-ui/react-slot';
-import { cva, type VariantProps } from 'class-variance-authority';
+'use client';
 
-import { cn } from '@/lib/utils';
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useRouter } from "next/navigation";
 
-const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
-  {
-    variants: {
-      variant: {
-        default:
-          'bg-primary text-primary-foreground shadow hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
-        outline:
-          'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
-        secondary:
-          'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
-      },
-      size: {
-        default: 'h-9 px-4 py-2',
-        sm: 'h-8 rounded-md px-3 text-xs',
-        lg: 'h-10 rounded-md px-8',
-        icon: 'h-9 w-9',
-      },
-    },
-    defaultVariants: {
-      variant: 'default',
-      size: 'default',
-    },
-  }
-);
-
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
+interface ButtonProps {
+  landingPage?: boolean;
+  themeDark?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button';
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    );
-  }
-);
-Button.displayName = 'Button';
+export function Button({ landingPage, themeDark }: ButtonProps) {
+  const router = useRouter();
+  const { theme, setTheme } = useTheme();
 
-export { Button, buttonVariants };
+  return (
+    <>
+      {themeDark && (
+        <button
+          type="button"
+          onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+          className="relative inline-flex h-9 w-9 items-center justify-center rounded-md transition-colors hover:border"
+        >
+          <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        </button>
+      )}
+      
+      {landingPage && (
+        <button
+          type='button'
+          className='ml-5 text-center w-1/3 p-2 rounded-full  bg-background hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:text-white active:scale-90'
+          onClick={() => router.push('/tecnologia')}>
+          Start
+        </button>
+      )}
+    </>
+  )
+}
