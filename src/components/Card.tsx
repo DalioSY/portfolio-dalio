a75dc067a0de5d166e6f6f44e0c35a5614c9fa53
DalioSY <dalioyamada@gmail.com>
2026-07-08 22:28:10 -0300

    responsivo

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,10 +11,10 @@ type ProjectProps = {
 export function Card({ item }: ProjectProps) {
 
   const branch = item.default_branch
-  
+
   return (
     <div className='flex flex-col justify-between p-4 gap-3 rounded border bg-background'>
-      <div className='w-full h-28  flex items-center justify-center'>
+      <div className='w-full h-28 flex items-center justify-center'>
         <img
           src={`https://raw.githubusercontent.com/DalioSY/${item.name}/${branch}/img/git-1.png`}
           alt='foto projeto'
@@ -50,16 +50,18 @@ export function Card({ item }: ProjectProps) {
           href={item.html_url}
           target='_blank'
         >
-          <Github className='h-5 w-5'/>
+          <Github className='h-5 w-5' />
           GitHub
         </a>
         <a
-          className={` flex items-center gap-1 text-ms border bg-background rounded-full py-1 px-3 hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:text-white active:scale-90 ${item.homepage === null ? 'hidden' : 'item.homepage'
-            } ${item.homepage === '' ? 'hidden' : 'item.homepage'}`}
+          className={` flex items-center gap-1 text-ms border bg-background rounded-full py-1 px-3 hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:text-white active:scale-90 
+            ${item.homepage === null ? 'hidden' : 'item.homepage'} 
+            ${item.homepage === '' ? 'hidden' : 'item.homepage'}
+            `}
           href={item.homepage}
           target='_blank'
         >
-          <GlobeIcon className='h-5 w-5'/>
+          <GlobeIcon className='h-5 w-5' />
           Deploy
         </a>
       </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,20 +4,20 @@ import { Button } from './button';
 export function Header() {
   return (
     <header className='h-20 fixed top-0 z-40 w-full p-3 flex items-center justify-between bg-background'>
-      <div className='flex items-center gap-4'>
+      <div className='flex items-center gap-1 sm:gap-4'>
         <img
           className='h-14 w-14 rounded-full border-2 border-cyan-500'
           src='https://avatars.githubusercontent.com/u/152891031?v=4'
           alt='foto perfil'
         />
         <div>
-          <h1 className="text-2xl font-bold bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent">Dalio S. Yamada</h1>
-          <span className='text-sm text-muted-foreground'>
+          <h1 className="sm:text-2xl font-bold bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent">Dalio S. Yamada</h1>
+          <span className='text-[8px] sm:text-sm text-muted-foreground'>
             Developer Full Stack
           </span>
         </div>
       </div>
-      <div className='flex items-center justify-center gap-4 '>
+      <div className='flex items-center justify-center gap-1 sm:gap-4 '>
         <a
           className='text-muted-foreground p-2 hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:text-white active:scale-90 hover:rounded-full'
           href='https://www.linkedin.com/in/dalio-s-yamada/'
